bd24c20c8d3d1c2b89d546f41dabfab02d615dac
vanit <vanit@192.168.1.18>
2017-12-16 14:34:09 -0800

    sdfdsc

--- Git_Demo/src/GitDemo.java
+++ Git_Demo/src/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	{
 		System.out.println("Hello World");
 		
-		System.out.println("Bye World");
+		
 		System.out.println("Not Working");
 		System.out.println("Hello World");
 		System.out.println("Next Commit");
