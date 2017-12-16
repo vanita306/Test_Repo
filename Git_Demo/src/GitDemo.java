f57418823561051c8b9db5fc8150bcc27d10cee9
vanit <vanit@192.168.1.18>
2017-12-16 14:04:43 -0800

    changes not working

--- Git_Demo/src/GitDemo.java
+++ Git_Demo/src/GitDemo.java
@@ -6,6 +6,7 @@ public class GitDemo {
 		System.out.println("Hello World");
 		System.out.println("Next Commit");
 		System.out.println("Bye World");
+		System.out.println("Not Working");
 		
 		
 	}
